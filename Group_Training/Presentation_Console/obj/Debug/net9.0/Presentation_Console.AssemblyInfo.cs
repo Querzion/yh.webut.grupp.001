1f031143517a9cc68c97988897c517db8f5df987
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2025-03-05 12:23:11 +0100

    added data class library

--- Group_Training/Presentation_Console/obj/Debug/net9.0/Presentation_Console.AssemblyInfo.cs
+++ Group_Training/Presentation_Console/obj/Debug/net9.0/Presentation_Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation_Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71a51fc9e5fa664fb2a0d9d78d2f28dfc3a18408")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b8765f81ce5fbcd0b1374d28a4e99955b49fe2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation_Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation_Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
